5dc2e096947341315e6240157b76621ff48800d2
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2024-08-08 21:41:37 +0300

    full config

--- web_app/db/config.py
+++ web_app/db/config.py
@@ -1,25 +1,20 @@
+import os
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    PG_USER: str = "postgres"
-    PG_PASS: str = "password"
-    PG_NAME: str = "db"
-    PG_HOST: str = "0.0.0.0"
-    PG_PORT: str = "8111"
+    PG_USER: str = os.getenv("PG_USER")
+    PG_PASS: str = os.getenv("PG_PASS")
+    PG_NAME: str = os.getenv("PG_NAME")
+    PG_HOST: str = os.getenv("PG_HOST")
+    PG_PORT: str = os.getenv("PG_PORT")
 
     echo: bool = True
 
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
 
 settings = Settings()
-
-
-# import os
-
-
-# class Settings(BaseSettings):
-#     PG_USER: str = os.getenv("PG_USER")
-#     PG_PASS: str = os.getenv("PG_PASS")
-#     PG_NAME: str = os.getenv("PG_NAME")
-#     PG_HOST: str = os.getenv("PG_HOST")
-#     PG_PORT: str = os.getenv("PG_PORT")
