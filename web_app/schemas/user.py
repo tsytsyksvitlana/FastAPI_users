bf7037d78b32718c238b8610ac5b3cc4b77e293d
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2024-08-29 13:10:09 +0300

    password validation changed

--- web_app/schemas/user.py
+++ web_app/schemas/user.py
@@ -10,7 +10,13 @@ class UserS(BaseModel):
     password: str
 
     PASSWORD_REGEX: ClassVar[re.Pattern] = re.compile(
-        r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d@\"'<>\s]).{8,24}$"
+        r"^"
+        r"(?=.*[a-zA-Zа-яА-Я])"
+        r"(?=.*[a-zа-я])"
+        r"(?=.*[A-ZА-Я])"
+        r"(?=.*\d)"
+        r"(?=.*[^\w\s@\"'<>\-])"
+        r".{8,24}$"
     )
 
     @field_validator("password")
@@ -19,7 +25,7 @@ class UserS(BaseModel):
             raise HTTPException(
                 status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
                 detail="Password must be 8-24 characters long, contain digits, "
-                "lowercase and uppercase letters, and special characters "
-                "except for @, \", ', <, >.",
+                "lowercase and uppercase letters of any alphabet, "
+                "and special characters except for @, \", ', <, >.",
             )
         return v
